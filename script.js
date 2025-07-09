3626877a7787a6d5489022fb0e89404d081c56a8
Prakashbhai9199 <prakashbhai070707@gmail.com>
2025-07-10 01:22:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function startCelebration() {
+    const music = document.getElementById('bg-music');
+    music.play();
+    confettiCanvas();
+}
+
+function confettiCanvas() {
+    const duration = 10 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    const interval = setInterval(function () {
+        const timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        const particleCount = 50 * (timeLeft / duration);
+        confetti(Object.assign({}, defaults, { particleCount, origin: { x: Math.random(), y: Math.random() } }));
+    }, 250);
+}
